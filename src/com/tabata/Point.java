1150b3990d7bbb1688eb455353946a79cc1e6eb9
tllach <tllach@uninorte.edu.co>
2022-11-10 11:13:56 -0500

    separation of classes made

---
+++ src/com/tabata/Point.java
@@ -0,0 +1,21 @@
+package com.tabata;
+
+class Point{
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+}
